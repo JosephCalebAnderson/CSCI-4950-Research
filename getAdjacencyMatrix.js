83a8a28fa1c7ffeb66db59d9e29d3d095d1aae7f
JosephCalebAnderson <jca47319@uga.edu>
2024-01-31 20:56:55 -0500

    fix comments from previous commit

--- getAdjacencyMatrix.js
+++ getAdjacencyMatrix.js
@@ -186,11 +186,14 @@ function getAdjacencyMatrix (stateArray) {
     for (let a = 0; a < count; a ++) {
         //index the states along the matrix
         adjMatrix[0][a + 2] = stateArray[a];
-        // CHANGE THE FOLLOWING LINE TO 0 FOR TAKE LAST AND 1 TO GIVE LAST.
-        adjMatrix[a + 1][0] = 0
+        adjMatrix[a + 1][0] = 0;
         adjMatrix[a + 1][1] = stateArray[a];
     }
-    // CHANGE THIS FOLLOWING LINE TO 1 FOR TAKE LAST AND -1 FOR GIVE LAST.
+    // changes MEX value for 0.
+    // CHANGE THE FOLLOWING LINE TO 0 FOR TAKE LAST TO WIN AND -1 TO GIVE LAST TO WIN.
+    adjMatrix[count][0] = 0
+    // changes MEX value for 1.
+    // CHANGE THIS FOLLOWING LINE TO 1 FOR TAKE LAST TO WIN AND 0 FOR GIVE LAST TO WIN.
     adjMatrix[count - 1][0] = 1;
     //console.log('');
     // the rows will identify the state we are starting from
@@ -288,7 +291,7 @@ function driver (n) {
     let winningStatesString = '';
     for (let state = 0; state < statesAndMex.length; state ++) {
         // Impose conditions on the winning states here.
-        if (statesAndMex[state][1] == 0 && statesAndMex[state][0].length == 2) {
+        if (statesAndMex[state][1] == 0) {
             winningStates.push(statesAndMex[state]);
             winningStatesString = winningStatesString + JSON.stringify(statesAndMex[state]) + '\n';
         }
@@ -304,6 +307,6 @@ function driver (n) {
 }
 
 // 30 does not crash. Crashes at 40
-driver(35);
+driver(9);
  
 // This code is contributed by divyesh072019
